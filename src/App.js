7c8acc64c6deffd597a7431ca4c5c070ae1b2003
Ashwin20102000 <jkashwin2@gmail.com>
2021-08-31 18:14:39 +0530

    EditUpdate

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 import './style.css';
 
 export default function App() {
-  let details = [
+  const [details, setDetails] = useState([
     {
       name: 'Ashwin',
       type: 'Shy',
@@ -29,38 +29,85 @@ export default function App() {
       type: 'Dedication',
       amount: '0'
     }
-  ];
+  ]);
   const [state, setState] = useState('0');
   const [form, setForm] = useState(false);
-  const { register , handleSubmit , formState:{errors}} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  console.log(state);
+  const onSubmit = data => {
+    console.log(data);
+     setDetails(()=>{
+      return (details[state]=data)
+     })
+    setForm(false);
+  };
+  console.log(details[state]);
   return (
     <div className="m-5 ">
       {form && (
-        <div className="editForm border border-success rounded">
-          <div className="header d-flex justify-content-around">
+        <div className="editForm container border border-success rounded">
+          <div className="header p-2 d-flex justify-content-around">
             <h3>Edit Form</h3>
             <button
-              onClick={()=> {
+              onClick={() => {
                 setForm(false);
               }}
-              className="btn btn-light text-dark"
+              className="btn btn-light text-danger"
             >
-              ✖
+              Close ✖
             </button>
           </div>
           <hr />
-          <form >
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-group d-flex flex-column justify-content-center px-5">
-            <label> Name <input {...register('name',{required:true})} className="form-control" type="text" placeholder="Enter Name"  id="" /></label>
-            <label> Type <input className="form-control" type="text" placeholder="Enter Type"  id="" /></label>
-            <label> Amount <input className="form-control" type="text" placeholder="Enter Amount"  id="" /></label>
+              <label>
+                {' '}
+                Name{' '}
+                <input
+                  {...register('name', { required: true })}
+                  className="form-control"
+                  type="text"
+                  placeholder="Enter Name"
+                  id=""
+                />
+                {errors.name && <p>*Name is required!</p>}
+              </label>
+              <label>
+                {' '}
+                Type{' '}
+                <input
+                  {...register('type', { required: true })}
+                  className="form-control"
+                  type="text"
+                  placeholder="Enter Type"
+                  id=""
+                />
+                {errors.type && <p>*Type is required!</p>}
+              </label>
+              <label>
+                {' '}
+                Amount{' '}
+                <input
+                  {...register('amount', { required: true })}
+                  className="form-control"
+                  type="text"
+                  placeholder="Enter Amount"
+                  id=""
+                />
+                {errors.amount && <p>*Amount is required</p>}
+              </label>
             </div>
-            <button className="btn btn-success m-4 mt-1 ml-4">Save & Continue</button>
+            <button className="btn btn-success m-4 mt-1 ml-4">
+              Save & Continue
+            </button>
           </form>
-          
         </div>
       )}
-      <table class="table">
+      <table class="table container">
         <thead>
           <tr>
             <th scope="col">Name</th>
@@ -74,7 +121,7 @@ export default function App() {
               <tr>
                 <td>
                   <button
-                  key={i}
+                    key={i}
                     type="button"
                     class="btn btn-outline-light text-dark"
                     onClick={() => {
